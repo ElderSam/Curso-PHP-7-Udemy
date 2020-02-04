3ba7d305ef6ff6962a7beb0f3fcb1506d3dd879e
ElderSam <eldersamuel98@gmail.com>
2020-02-03 21:49:52 -0300

    cURL (requisições para API)

--- curl/exemplo-01.php
+++ curl/exemplo-01.php
@@ -1,6 +1,9 @@
 <?php
 
-/* o cURL é um "robô", que faz requisições de dados através de URL's e recebe esses dados */
+/* cURL - FERRAMENTA PARA TRANSFERÊNCIA DE DADOS (DE UMA API)
+https://www.php.net/manual/pt_BR/curl.examples-basic.php
+https://www.hostinger.com.br/tutoriais/comando-curl-linux/
+o cURL é um "robô", que faz requisições de dados através de URL para uma API, e recebe esses dados */
 
 $cep = "01310100";
 
@@ -8,7 +11,8 @@ $link = "viacep.com.br/ws/$cep/json/";
 
 $ch = curl_init($link); //biblioteca que iniciou o arquivo
 
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //curl_setopt(ch, option, value); SE O VALUE = 1 (TRUE) ENTÃO TEM RETORNO 
+//curl_setopt(ch, option, value) CONFIGURA OPÇÕES PARA TRANFERÊNCIA
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //SE O VALUE = 1 (TRUE) ENTÃO TEM RETORNO 
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); //validação 0 = false
 
 $response = curl_exec($ch); //executa o acesso para a URL desejada
