45111ffbdba8c37a023ff3b5ac6acc87ad916deb
ElderSam <eldersamuel98@gmail.com>
2020-02-13 23:16:28 -0300

    criando Virtual Hosts com o Apache

---
+++ virtualHost/index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+    Utilizando o Apache para criar Virtual Hosts (diretórios virtuais)
+
+    OBS: tem que configurar os arquivos;
+        C:\xampp\apache\conf\extra\httpd-hosts.conf
+        C:\Windows\System32\drivers\etc\hosts
+
+        se deu certo é só digitar na URL do navegadro o seu virtual host criado: local.meusite.com.br
+*/
+
+
+phpinfo();
